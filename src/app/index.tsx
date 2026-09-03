377d08733e2072a0bb7244601fd53386c6e25106
Teo Wen Kai <wenkai04@hotmail.com>
2026-09-03 15:06:33 +0800

    Map auto goes to user's precise location for ease of use

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,81 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Map } from "@maplibre/maplibre-react-native";
+import { Map, Camera, type CameraRef } from "@maplibre/maplibre-react-native";
+import { useEffect, useState, useRef } from "react";
+import * as Location from "expo-location";
+
+const DEFAULT_LOCATION: Location.LocationObject = {
+    coords: {
+      latitude: 1.290270,
+      longitude: 103.851959,
+      altitude: null,
+      accuracy: null,
+      altitudeAccuracy: null,
+      heading: null,
+      speed: null,
+    },
+    timestamp: Date.now(),
+  }
+
+const INITIAL_VIEW_STATE = {
+  center:[DEFAULT_LOCATION.coords.longitude, DEFAULT_LOCATION.coords.latitude] as [number, number],
+  zoom: 15,
+}
 
 export default function Index() {
+  const [location, setLocation] = useState<Location.LocationObject>(DEFAULT_LOCATION);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const cameraRef = useRef<CameraRef>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied.");
+          setLocation(DEFAULT_LOCATION);
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.BestForNavigation,
+        });
+        console.log("Got location: ", location.coords.latitude, location.coords.longitude);
+        setLocation(location);
+
+        // Move camera to curr location
+        try {
+          setTimeout(() => {
+            cameraRef.current?.jumpTo({
+              center: [location.coords.longitude, location.coords.latitude],
+            });
+            console.log("jumpTo was called");
+          }, 500)
+        } catch (e) {
+          console.warn("jumpTo threw: ", e);
+        }
+
+      } catch (error) {
+        console.warn("Location error: ", error);
+        setLocation(DEFAULT_LOCATION);
+      }
+    }
+    getCurrentLocation();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Map style={styles.map} mapStyle="https://demotiles.maplibre.org/style.json"/>
+      <Map style={styles.map} mapStyle="https://tiles.openfreemap.org/styles/liberty">
+        <Camera
+          ref={cameraRef} initialViewState={INITIAL_VIEW_STATE}
+        />
+      </Map>
     </View>
   );
 }
